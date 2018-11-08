1a2ebe478ff518018c1c69ad647a6fd2161d91fb
Nathaniel Worcester <nathaniel.worcester@gmail.com>
2018-11-08 12:40:05 -0600

    adding main.py with User class propperly linked also register template

--- main.py
+++ main.py
@@ -3,22 +3,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:localonly@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:testonly@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 
 app.secret_key ="KJ75259"
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    """docstring for BlogPost."""
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('BlogPost', backref='owner')
+
+    def __init__(self, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+
+
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     body =  db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+        self.id = id
+        self.owner = owner
 
 
 
@@ -54,7 +71,7 @@ def make_a_post():
 
 @app.route('/blog', methods = ['POST', 'GET'])
 
-def index():                                         #slimmed down rendering of the blog template
+def blog_view():                                         #slimmed down rendering of the blog template
 
     post_id = request.args.get('id')
     if post_id == None:
